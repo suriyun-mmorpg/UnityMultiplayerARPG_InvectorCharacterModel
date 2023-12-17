8ec8cbaa0235d12bbf4fdc1bb96a27b305f79037
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-12-17 17:17:54 +0700

    feat: implement recoiling

--- Scripts/InvectorShooterGameplayCameraController.cs
+++ Scripts/InvectorShooterGameplayCameraController.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 namespace MultiplayerARPG
 {
     public class InvectorShooterGameplayCameraController : InvectorGameplayCameraController, IShooterGameplayCameraController
     {
+        public float recoilRateX = 1f;
+        public float recoilRateY = 1f;
+        public float recoilRateZ = 1f;
+        public float recoilReturnSpeed = 2f;
+        public float recoilSmoothing = 6f;
+
         public bool EnableAimAssist { get; set; }
         public bool EnableAimAssistX { get; set; }
         public bool EnableAimAssistY { get; set; }
@@ -15,9 +23,22 @@ namespace MultiplayerARPG
         public float AimAssistMaxAngleFromFollowingTarget { get; set; }
         public float CameraRotationSpeedScale { get; set; }
 
-        public void Recoil(float x, float y)
+        private Vector3 _targetRecoilRotation;
+        private Vector3 _currentRecoilRotation;
+
+        public void Recoil(float x, float y, float z)
         {
-            invectorCam.RotateCamera(x, y);
+            _targetRecoilRotation += new Vector3(x * recoilRateX, y * recoilRateY, z * recoilRateZ);
+        }
+
+        protected override void LateUpdate()
+        {
+            base.LateUpdate();
+            // Update recoiling
+            float deltaTime = Time.deltaTime;
+            _targetRecoilRotation = Vector3.Lerp(_targetRecoilRotation, Vector3.zero, deltaTime * recoilReturnSpeed);
+            _currentRecoilRotation = Vector3.Lerp(_currentRecoilRotation, _targetRecoilRotation, deltaTime * recoilSmoothing);
+            invectorCam.transform.eulerAngles += _currentRecoilRotation;
         }
     }
 }
